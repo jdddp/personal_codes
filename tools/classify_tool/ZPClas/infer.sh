0c237744f8ef4e6195a924fa1decc8bc1e36c532
jdddp <821885947@qq.com>
2022-03-18 22:40:19 +0800

    modify infer and add eval

--- tools/classify_tool/ZPClas/infer.sh
+++ tools/classify_tool/ZPClas/infer.sh
@@ -1,3 +1,3 @@
-python main.py -m shufflenet_v2_x0_5 -b 10 -n 10 -i E:/vsCodes/personal_codes/test -p E:/vsCodes/personal_codes/tools/classify_tool/projects/dogClasV1 -t test -a 1
+python main.py -m shufflenet_v2_x0_5 -b 10 -n 10 -i E:/vsCodes/personal_codes/test -p E:/vsCodes/personal_codes/tools/classify_tool/projects/dogClasV1 -t test -a 3
 shufflenet_v2_x0_5
 E:/vsCodes/personal_codes/tools/classify_tool/projects/dogClasV1/dogV1/hashiqi/0a3647ba-4250-441d-8ab8-2661c7d26782.jpg
