024bcc8335053e08b3512ccb7bee3b62b94a568d
jdddp <821885947@qq.com>
2022-04-11 17:21:23 +0800

    img2jpg

---
+++ common_use/image_format.py
@@ -0,0 +1,58 @@
+from distutils.log import info
+import os
+import os.path as osp
+import sys
+import cv2
+from PIL import Image
+from tqdm import tqdm
+import uuid
+import numpy as np
+import math
+import multiprocessing
+import io
+from io import BytesIO
+from common_use import get_imagelist, makeDir
+
+def preprocess(info_tp):
+    imgpath, short_size, dstpath=info_tp
+    img=cv2.imread(imgpath)
+    # img=cv2.cvtColor(cv2.imread(imgpath), cv2.COLOR_BGR2RGB)
+
+    if short_size != 0:
+        h,w,_=img.shape
+        if min(h,w)>short_size:
+            ratio=float(short_size)/min(h,w)
+            w_n,h_n=math.floor(w*ratio),math.floor(h*ratio)
+            img=cv2.resize(img, (w_n,h_n))
+    
+    cv2.imwrite(dstpath, img)
+
+def formatDir(dirpath, outdir, format='dir', \
+    short_size=0, reserve_name=0):
+    imglist=get_imagelist(dirpath,format)
+    print('get images %d'% len(imglist))
+    makeDir(outdir)
+    short_size, reserve_name=int(short_size), int(reserve_name)
+    if reserve_name==0:
+        info_tp=[(p, short_size, osp.join(outdir,osp.basename(p).split('.')[0]+'.jpg')) \
+            for p in imglist]
+    else:
+        info_tp=[(p, short_size, osp.join(outdir,str(uuid.uuid1())+'.jpg')) \
+            for p in imglist]
+    
+    pool=multiprocessing.Pool(8)
+    for _ in tqdm(pool.imap_unordered(preprocess, info_tp)):
+        pass
+
+#test
+def format(imgpath, outdir):
+    short_size=0
+    info_tp=(imgpath, short_size, osp.join(outdir, str(uuid.uuid1())+'.jpg'))
+    preprocess(info_tp)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 :
+        func = getattr(sys.modules[__name__], sys.argv[1])
+        func(*sys.argv[2:])
+    else:
+        print('wrong!')
