80ce2f9415d5370e6ef87e6fd94f46bc0ae23646
jdddp <821885947@qq.com>
2022-04-13 23:02:57 +0800

    draw for miss_mistake

--- tools/visualize_tools/draw_bbox.py
+++ tools/visualize_tools/draw_bbox.py
@@ -8,12 +8,12 @@ import numpy as np
 from PIL import Image, ImageFont, ImageDraw
 
 
-def draw_bbox(src_dir,dst_dir,img_name,res):
+def draw_bbox_with_score(src_dir,dst_dir,img_name,res):
     '''res->dict;三个字段匹配usualJson;bbox[xywh]、cate、score
     '''
     cv2_im = cv2.imread(osp.join(src_dir,img_name))
     h,w,_ = cv2_im.shape
-    width = 2 if h*w >= 200*200 else 3
+    wid_th = 4 if h*w >= 200*200 else 3
     source_img = Image.fromarray(cv2.cvtColor(cv2_im,cv2.COLOR_BGR2RGB)) 
     draw = ImageDraw.Draw(source_img)
 
@@ -23,7 +23,7 @@ def draw_bbox(src_dir,dst_dir,img_name,res):
         bbox = lab['bbox']
         print(label,label.decode('utf-8'),label.decode('unicode-escape'))
         color = tuple(np.random.randint(256, size=3))
-        draw.rectangle(((bbox[0], bbox[1]), (bbox[2] + bbox[0], bbox[3] + bbox[1])),width=width+1,outline =color)
+        draw.rectangle(((bbox[0], bbox[1]), (bbox[2] + bbox[0], bbox[3] + bbox[1])),width=wid_th+1,outline =color)
         draw.text((bbox[0], bbox[1]), '{}_{:.2}'.format(label,score),fill=color, font=ImageFont.truetype("C:/Windows/Fonts/msyh.ttc"))
     source_img.save(osp.join(dst_dir,img_name))
 
@@ -57,6 +57,37 @@ def draw_imgs(dirpath):
             source_img.save(osp.join(ansDir, imgname))
             # except:
             #     print(imgname)
+
+#for ../../detection_tools/miss_mistake
+def draw_miss_mistake(imgDir,miss_mistake_json,goalDir):
+    os.makedirs(goalDir, exist_ok=True)
+    res=json.loads(open(miss_mistake_json).read())
+    miss_color=tuple(np.random.randint(256, size=3))
+    mistake_color=tuple(np.random.randint(256, size=3))
+    for imgname,img_info in res.items():
+        if len(img_info['miss'])==0 and len(img_info['mistake'])==0:
+            continue
+        cv2_im = cv2.imread(osp.join(imgDir,imgname))
+        h,w,_ = cv2_im.shape
+        wid_th = 4 if h*w >= 200*200 else 3
+        source_img = Image.fromarray(cv2.cvtColor(cv2_im,cv2.COLOR_BGR2RGB)) 
+        draw = ImageDraw.Draw(source_img)
+
+        for sgDct in img_info['mistake']:
+            label = sgDct['category']
+            score = sgDct['score']
+            bbox = sgDct['bbox']
+            draw.rectangle(((bbox[0], bbox[1]), (bbox[2] + bbox[0], bbox[3] + bbox[1])),width=wid_th+1,outline =mistake_color)
+            draw.text((bbox[0], bbox[1]), '{}_{:.2}'.format(label,score),fill=mistake_color, font=ImageFont.truetype("C:/Windows/Fonts/msyh.ttc", 35))
+        
+        for sgDct in img_info['miss']:
+            label = sgDct['category']
+            # score = sgDct['score']
+            bbox = sgDct['bbox']
+            draw.rectangle(((bbox[0], bbox[1]), (bbox[2] + bbox[0], bbox[3] + bbox[1])),width=wid_th+1,outline =miss_color)
+            draw.text((bbox[0], bbox[1]), '{}'.format(label),fill=miss_color, font=ImageFont.truetype("C:/Windows/Fonts/msyh.ttc", 35))
+
+        source_img.save(osp.join(goalDir,imgname))
 
 if __name__=='__main__':
     if len(sys.argv)>1:
