b32f49d99d57cee785c48562795cc8980c747edd
jdddp <821885947@qq.com>
2022-05-20 21:45:08 +0800

    get_lst

--- tools/format_img/format_dup.sh
+++ tools/format_img/format_dup.sh
@@ -3,5 +3,5 @@ python ./image_format.py formatDir \
 /home/linke/codes/localCodes/dataset/LH/LHV2/LH_plus \
 /home/linke/codes/localCodes/dataset/LH/LHV2/LH_2
 
-# python ./duplicate_imgs.py duplicate_dir \
-# /home/linke/codes/localCodes/dataset/LH/LHV2/LH_2
+python ./duplicate_imgs.py duplicate_dir \
+/home/linke/codes/localCodes/dataset/LH/LHV2/LH_2
