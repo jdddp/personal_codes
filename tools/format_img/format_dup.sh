6df99ded36c257b18844c68c217bccc8f36ca14a
jdddp <821885947@qq.com>
2022-05-20 19:42:56 +0800

    quick_use

---
+++ tools/format_img/format_dup.sh
@@ -0,0 +1,7 @@
+#sg
+python ./image_format.py formatDir \
+E:\vsCodes\a_project_com\dataV2
+E:\vsCodes\detection_tools\dataset\datasetLHv2
+
+python ./duplicate_imgs.py duplicate_dir \
+E:\vsCodes\detection_tools\dataset\datasetLHv2
